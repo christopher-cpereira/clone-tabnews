66566feb24d7f6098c14cced5a44061a5775534f
christopher-cpereira <christopher.cpereira@gmail.com>
2025-12-09 06:48:53 -0300

    Alteração básica para testar dentro do site da versel o deplayment continuos, já tem uma versão mais básica, agora vamos atualiza-lá!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
  function Home(){
-    return <h1>Mensagem de agradecimento para as pessoas que acreditam em mim!</h1>
+    return <h1>Mensagem de agradecimento para as pessoas que acreditam em mim! agora com uma leve alteração para testes de deplay com o site da versel</h1>
  }
 
  export default Home;
