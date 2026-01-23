9e8b6992ff13f51d2710472fd7d9e31918454c49
christopher-cpereira <christopher.cpereira@gmail.com>
2026-01-22 21:58:57 -0300

    atualizando os arquivos com o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
- function Home(){
-    return <h1>Mensagem de agradecimento para as pessoas que acreditam em mim! agora com uma leve alteração para testes de deplay com o site da versel</h1>
- }
+function Home() {
+  return (
+    <h1>
+      Mensagem de agradecimento para as pessoas que acreditam em mim! agora com
+      uma leve alteração para testes de deplay com o site da versel
+    </h1>
+  );
+}
 
- export default Home;
+export default Home;
