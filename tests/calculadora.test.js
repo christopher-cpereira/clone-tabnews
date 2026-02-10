80c9b3836d6907877e2740102a5409cfec1d367d
christopher-cpereira <christopher.cpereira@gmail.com>
2026-02-10 20:34:11 -0300

    adicionando alguns exemplos de testes automatizados

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,21 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'teste' + 100 deveria retornar 'Error'", () => {
+  const resultado = calculadora.somar("teste", 100);
+  expect(resultado).toBe("Error");
+});
+
+test("somar 100 + 'teste' deveria retornar 'Error'", () => {
+  const resultado = calculadora.somar(100, "teste");
+  expect(resultado).toBe("Error");
 });
